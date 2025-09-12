1c878b1b0f320e0438e963a1f3c54ff8e37bd455
Lais <amano.lais90@gmail.com>
2025-09-11 22:38:07 -0300

    detalhes

--- js/topbar.js
+++ js/topbar.js
@@ -4,7 +4,6 @@ function topbar() {
             <form class="barra-de-busca" action="#" method="get">
                 <img class="icone-lupa" src="./img/icone-lupa.svg" alt="imagem de lupa na barra de pesquisa">
                 <input type="text" class="espaco-pesquisar" placeholder="Buscar jogo ou palavra chave" name="query">
-                <button type="submit" class="botao-pesquisar">Buscar</button>
             </form>
 
             <img class="user-icon" src="./img/user-icon.png" alt="foto de perfil do usuario">
