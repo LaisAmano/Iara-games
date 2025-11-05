cb6bcbec17a097fa11665c61a0c0a58eaa881d23
Lais <amano.lais90@gmail.com>
2025-11-04 22:18:37 -0300

    bootstrap na side bar 90% completo

--- js/menu-hamburguer.js
+++ js/menu-hamburguer.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const btn = document.getElementById("btnHamburger");
-    const sidebar = document.getElementById("sidebar");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const btn = document.getElementById("btnHamburger");
+//     const sidebar = document.getElementById("sidebar");
 
-    btn.addEventListener("click", function () {
-        const isVisible = window.getComputedStyle(sidebar).display !== "none";
-        sidebar.style.display = isVisible ? "none" : "flex";
-    });
-});
+//     btn.addEventListener("click", function () {
+//         const isVisible = window.getComputedStyle(sidebar).display !== "none";
+//         sidebar.style.display = isVisible ? "none" : "flex";
+//     });
+// });
 
-const btnHamburger = document.querySelector('.btn-hamburger');
-if (btnHamburger) {
-    document.body.appendChild(btnHamburger);
-}
+// const btnHamburger = document.querySelector('.btn-hamburger');
+// if (btnHamburger) {
+//     document.body.appendChild(btnHamburger);
+// }
